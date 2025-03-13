35bb81e596c1d569eda5296843de4025f0c81308
piyush <piyush.priyadarshi@antrepriz.com>
2025-03-14 02:59:20 +0530

    Refactor the StringCalculator method

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,14 +1,20 @@
 function StringCalculator(s1) {
   if (s1) {
     let delimiter = /[\n,]/;
+  if (s1.startsWith("//")) {
+    const match = s1.match(/^\/\/(.+)\n/);
+    if (match) {
+      delimiter = new RegExp(`[${match[1]}]`);
+      s1 = s1.slice(match[0].length); 
+    }
+  }
     const numbers = s1.split(delimiter).map(Number);
     const negatives = numbers.filter((num) => num < 0);
     if (negatives.length > 0) {
       throw new Error(`negatives not allowed: ${negatives.join(', ')}`);
     }
-    let s2 = s1.replace(/\W/g, ","); // this is working for all different delimiters
-    let arr = s2.split(",");
-    return arr.reduce((sum, cur) => sum + Number(cur), 0);
+    
+    return numbers.reduce((sum, cur) => sum + Number(cur), 0);
   }
   return 0;
 }
