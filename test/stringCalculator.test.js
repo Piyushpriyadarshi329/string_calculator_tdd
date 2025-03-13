339da706d718ff0d0df0f8a3b6fd3e1103141760
piyush <piyush.priyadarshi@antrepriz.com>
2025-03-14 02:01:14 +0530

    write 2nd test case 'should return the number for a single number input'

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -5,5 +5,8 @@ describe("StringCalculator test",()=>{
     it('should return 0 for an empty string', () => {
         expect(StringCalculator('')).toBe(0);
     })
+    it('should return the number for a single number input', () => {
+        expect(StringCalculator('1')).toBe(1);
+    })
 
 })
