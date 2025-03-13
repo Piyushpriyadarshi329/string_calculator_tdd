6cef6ec0c1ceb825d68faa4511cc704e14f94afc
piyush <piyush.priyadarshi@antrepriz.com>
2025-03-14 02:09:09 +0530

    pass 3rd test case 'should return the sum of two numbers'

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,5 +1,8 @@
 function StringCalculator(s1) {
-  if (s1) return Number(s1);
+  if (s1) {
+    let arr = s1.split(",");
+    return arr.reduce((sum, cur) => sum + Number(cur), 0);
+  }
   return 0;
 }
 module.exports = { StringCalculator };
