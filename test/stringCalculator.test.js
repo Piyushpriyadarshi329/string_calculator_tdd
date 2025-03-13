43c813ea4638baf31ebd58bcaa3dfeb70ec92b94
piyush <piyush.priyadarshi@antrepriz.com>
2025-03-14 02:06:02 +0530

    write 3rd test case 'should return the sum of two numbers'

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -1,12 +1,13 @@
 const { StringCalculator } = require("../src/StringCalculator");
 
-
-describe("StringCalculator test",()=>{
-    it('should return 0 for an empty string', () => {
-        expect(StringCalculator('')).toBe(0);
-    })
-    it('should return the number for a single number input', () => {
-        expect(StringCalculator('1')).toBe(1);
-    })
-
-})
+describe("StringCalculator test", () => {
+  it("should return 0 for an empty string", () => {
+    expect(StringCalculator("")).toBe(0);
+  });
+  it("should return the number for a single number input", () => {
+    expect(StringCalculator("1")).toBe(1);
+  });
+  it("should return the sum of two numbers", () => {
+    expect(StringCalculator("1,2")).toBe(3);
+  });
+});
