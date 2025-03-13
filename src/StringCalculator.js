ba0adc394436cb7c59e88d673aa3988609f5f29a
piyush <piyush.priyadarshi@antrepriz.com>
2025-03-14 02:22:06 +0530

    pass 'Support different delimiters" test case

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,6 +1,6 @@
 function StringCalculator(s1) {
   if (s1) {
-    let s2=s1.replace(/\W/g, ',')  
+    let s2=s1.replace(/\W/g, ',')   // this is working for all different delimiters
     let arr = s2.split(",");
     return arr.reduce((sum, cur) => sum + Number(cur), 0);
   }
